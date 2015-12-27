d26fdacb07508b3a36505bfc1236ad722c3deaf8
Dominique Barton <dbarton@confirm.ch>
2015-12-27 22:22:35 +0100

    DOCS: Removed Markdown parsing in sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,12 +37,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = ['.rst', '.md']
-
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-    '.md': CommonMarkParser
-}
+source_suffix = ['.rst']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
